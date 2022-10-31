34c36392408da3fbf50426b158bf0a7541cba141
Marvin Hill <86198682+MarvinHill@users.noreply.github.com>
2022-10-31 10:55:30 +0100

    removed save

--- src/main/java/PermParity.java
+++ src/main/java/PermParity.java
@@ -12,64 +12,18 @@ import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class PermParity {
-  static final Scanner scanner = new Scanner(System.in);
-  static final HashMap<Integer, LinkedList<Integer[]>> speicher = new HashMap<>();
-  static final Path path = Paths.get("src/main/java/data.txt");
-  static boolean save = false;
+  static final Scanner scanner = new Scanner(System.in); //für die Eingabe von n
   static long count = 0;
 
   public static void main(String[] args) throws IOException {
-    if (save) Files.deleteIfExists(path);
-    if (save) Files.createFile(path);
     // start application main loop
     while (true) {
       // get user input
       System.out.print("Please enter n: ");
       int n = scanner.nextInt();
       System.out.println("new n: " + n);
-      if (save) Files.writeString(path, "n = " + n + "\n", StandardOpenOption.APPEND);
       computeAllParity(n);
-      //measureTime(n);
     }
-  }
-
-  private static void measureTime(int n) throws IOException {
-    ArrayList<Long> times = new ArrayList<>();
-    for (int i = 1; i <= n; i++) {
-      long startTime = System.nanoTime();
-      computeAllParity(i);
-      long endTime = System.nanoTime();
-      long duration = (endTime - startTime);
-      long durationInMs = TimeUnit.MILLISECONDS.convert(duration, TimeUnit.NANOSECONDS);
-      times.add(durationInMs);
-    }
-
-    double maxTime = 0;
-    for (double time : times) {
-      if (time > maxTime) maxTime = time;
-    }
-    double x = 100 / maxTime;
-
-    System.out.println("Performance:");
-    if (save) Files.writeString(path, "Performance:\n", StandardOpenOption.APPEND);
-    int count = 1;
-    for (double time : times) {
-      int amount = (int) (time * x);
-      if (save) Files.writeString(path, "n = " + Integer.toString(count) + " |", StandardOpenOption.APPEND);
-      System.out.print("n = " + count++ + " |");
-      if (amount == 0) {
-        if (save) Files.writeString(path, "*", StandardOpenOption.APPEND);
-        System.out.print("*");
-      }
-      for (int i = 0; i < amount; i++) {
-        if (save) Files.writeString(path, "*", StandardOpenOption.APPEND);
-        System.out.print("*");
-      }
-      if (save) Files.writeString(path, " " + time + " ms\n", StandardOpenOption.APPEND);
-      System.out.println(" " + time + " ms");
-    }
-    System.out.println("All times: " + times);
-    if (save) Files.writeString(path, "All times: " + times + "\n", StandardOpenOption.APPEND);
   }
 
   private static void computeAllParity(int n) throws IOException {
@@ -98,8 +52,9 @@ public class PermParity {
 
   private static void computeParity(ArrayList<Integer> list, ArrayList<Integer> rest, int n)
       throws IOException {
+    //Wenn Rest leer, da keine Zahlen mehr und Ergebnis lang genug
     if (rest.size() == 0 && list.size() <= n) {
-      if (save) Files.writeString(path, list.toString() + "\n", StandardOpenOption.APPEND);
+      //dann wird das Ergebnis ausgegeben
       System.out.println(list);
       count++;
       return;
